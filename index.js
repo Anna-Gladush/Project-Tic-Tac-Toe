d9b1e62339ac238e44cad5573e36bef74c779336
Anna-Gladush <ann.gladush@gmail.com>
2026-03-04 17:33:12 +0200

    Added turn function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // game, player or gameboard objects
+function Gameboard() {
+  let gameboard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+  const turn = (marker, number) => {
+    gameboard.splice(number - 1, 1, marker);
+    console.log(gameboard);
+  }
+  const game = () => {
+    turn('X', 4);
+  }
+}
+// Gameboard.game
+function Player(marker) {
+  // let win = 0;
+  // let tie = 0;
+  // let loss = 0;
+  // marker;
+}
