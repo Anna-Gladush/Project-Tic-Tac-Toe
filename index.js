2cd19b086b875cb6e0ca33a43144072a0cce8b81
Anna-Gladush <ann.gladush@gmail.com>
2026-03-04 18:22:25 +0200

    Added buttons to HTML, and linked css and script

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // game, player or gameboard objects
-function Gameboard() {
-  let gameboard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-  const turn = (marker, number) => {
-    gameboard.splice(number - 1, 1, marker);
-    console.log(gameboard);
+const Gameboard = (function () {
+  let gameboard = [[null, null, null], [null, null, null], [null, null, null]];
+  const turn = (marker, row, column) => {
+    // if (gameboard[row][column] !== null) {return ...smth...}
+    gameboard[row][column] = marker;
   }
-  const game = () => {
-    turn('X', 4);
-  }
-}
+})();
+
 // Gameboard.game
-function Player(marker) {
-  // let win = 0;
-  // let tie = 0;
-  // let loss = 0;
+const Player = (function(marker) {
   // marker;
-}
+  // let win = 0;
+  // let loss = 0;
+  // let tie = 0;
+})();
+
+const Game = (function() {
+  //smth
+})();
