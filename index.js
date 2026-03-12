53904f46060a9d97f8ee021c5f16488721bdc7f0
Anna-Gladush <ann.gladush@gmail.com>
2026-03-12 13:18:00 +0200

    Added event for clicking on cell and printing symbol

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const Gameboard = (function () {
       console.log(gameboard);
       win_check(marker);
       return true;
-      
     }
   }
   const reset = () => {
@@ -17,21 +16,32 @@ const Gameboard = (function () {
     console.log(gameboard);
   }
   const win_check = (marker) => {
-    // check if all cells are filled
     if (gameboard[0].indexOf(null) === -1 && gameboard[1].indexOf(null) === -1 && gameboard[2].indexOf(null) === -1) {
       console.log('Tie');
+      player.tie ++;
+      return true;
     } else if (gameboard[0] == [marker, marker, marker] || gameboard[1] == [marker, marker, marker] || gameboard[2] == [marker, marker, marker]) {
       console.log(`${marker} - win`);
+      player.win ++;
+      return true;
     } else if (gameboard[0][0] === marker && gameboard[0][1] === marker && gameboard[0][2] === marker) {
       console.log(`${marker} - win`);
+      player.win ++;
+      return true;
     } else if (gameboard[0][0] === marker && gameboard[1][0] === marker && gameboard[2][0] === marker) {
       console.log(`${marker} - win`);
+      player.win ++;
+      return true;
     } else if (gameboard[0][1] === marker && gameboard[1][1] === marker && gameboard[2][1] === marker) {
       console.log(`${marker} - win`);
+      player.win ++;
+      return true;
     } else if (gameboard[0][2] === marker && gameboard[1][2] === marker && gameboard[2][2] === marker) {
       console.log(`${marker} - win`);
+      player.win ++;
+      return true;
     }
-    // check if there is three in a row>
+    return false;
   }
   return {
     turn,
@@ -39,9 +49,27 @@ const Gameboard = (function () {
   }
 })();
 
-// const Player = (function(marker) {
-//   let marker = marker;
-//   let win = 0;
-//   let loss = 0;
-//   let tie = 0;
-// })();
+const Player = function(mark) {
+  let marker = mark;
+  let win = 0;
+  let tie = 0;
+  return {
+    marker,
+    win,
+    tie
+  }
+}
+
+function printSymbol(symbol) {
+  const cell = document.querySelectorAll('.cell');
+  cell.forEach((cube) => {
+    cube.addEventListener('click', () => {
+      if (symbol === 'X') {
+        cube.style.background  = "#A7E399 url('images/x-symbol.svg') no-repeat center center";
+      } else {
+        cube.style.background  = "#A7E399 url('images/o-symbol.svg') no-repeat center center";
+      }
+      cube.style.backgroundSize = '80px'
+    })
+  })
+}
