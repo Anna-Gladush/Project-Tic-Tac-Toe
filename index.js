f120da29fad6e2c3473c5b69289f56c8f5028458
Anna-Gladush <ann.gladush@gmail.com>
2026-03-16 10:51:46 +0200

    Organized DOM elements into module pattern, combined functions that print symbol and make a turn

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 // game, player or gameboard objects
+const DOMmanipulation = (() => {
+  const resetButtonClick = () => {
+    reset = document.querySelector('.btn-reset');
+    reset.addEventListener('click', () => {
+      Gameboard.reset();
+      cell.forEach((elem) => {
+        elem.style.background = "#A7E399";
+        elem.dataset.filled = 'no'
+      });
+    })
+  };
+  const audioEvent = (event) => {
+    if (event === 'win') {
+      key = 2;
+    } else if (event === 'tie') {
+      key = 1;
+    }
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if (key === 2) { audio.playbackRate = 2}
+    audio.play();
+  }
+  const turnSound = () => {
+    const turn = document.querySelector(`audio[data-key="3"]`);
+    cell.forEach((cube) => {
+      cube.addEventListener('click', () => {
+        turn.play();
+      })
+    })
+  }
+    const menu = () => {
+      start = document.querySelector('.start')
+      startBTN = document.querySelector('.btn-start')
+      startBTN.addEventListener('click', () => {
+        start.style.visibility = "hidden";
+    })
+  }
+  return {
+    resetButtonClick,
+    audioEvent,
+    turnSound,
+    menu
+  }
+})();
+
 const cell = document.querySelectorAll('.cell');
 const Gameboard = (function () {
   let gameboard = [[null, null, null], [null, null, null], [null, null, null]];
@@ -7,44 +51,43 @@ const Gameboard = (function () {
       return false;
     } else {
       gameboard[row][col] = marker;
+      // printSymbol(marker);
       console.log(gameboard);
       win_check(marker);
-      return true;
+      return marker;
     }
   }
   const reset = () => {
     gameboard = [[null, null, null], [null, null, null], [null, null, null]];
-    console.log(gameboard);
   }
   const win_check = (marker) => {
-    console.log(gameboard[1])
     if (gameboard[0].indexOf(null) === -1 && gameboard[1].indexOf(null) === -1 && gameboard[2].indexOf(null) === -1) {
       console.log('Tie');
-      audioEvent('tie');
+      DOMmanipulation.audioEvent('tie');
       return true;
     } else if (JSON.stringify(gameboard[0]) == JSON.stringify([marker, marker, marker]) || JSON.stringify(gameboard[1]) == JSON.stringify([marker, marker, marker]) || JSON.stringify(gameboard[2]) == JSON.stringify([marker, marker, marker])) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     } else if (gameboard[0][0] === marker && gameboard[1][1] === marker && gameboard[2][2] === marker) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     } else if (gameboard[0][2] === marker && gameboard[1][1] === marker && gameboard[2][0] === marker) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     } else if (gameboard[0][0] === marker && gameboard[1][0] === marker && gameboard[2][0] === marker) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     } else if (gameboard[0][1] === marker && gameboard[1][1] === marker && gameboard[2][1] === marker) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     } else if (gameboard[0][2] === marker && gameboard[1][2] === marker && gameboard[2][2] === marker) {
       console.log(`${marker} - win`);
-      audioEvent('win');
+      DOMmanipulation.audioEvent('win');
       return true;
     }
     return false;
@@ -66,59 +109,26 @@ const Player = function(mark) {
   }
 }
 
-function printSymbol(symbol) {
-  cell.forEach((cube) => {
-    cube.addEventListener('click', () => {
-      if (symbol === 'X') {
-        cube.style.background  = "#A7E399 url('images/x-symbol.svg') no-repeat center center";
-      } else if (symbol === 'O') {
-        cube.style.background  = "#A7E399 url('images/o-symbol.svg') no-repeat center center";
-      }
-      cube.style.backgroundSize = '80px'
-    })
-  })
-}
-
-function turnSound() {
-  const turn = document.querySelector(`audio[data-key="3"]`);
-  cell.forEach((cube) => {
-    cube.addEventListener('click', () => {
-      turn.play();
-    })
-  })
-}
-
-function audioEvent(event) {
-  if (event === 'win') {
-    key = 2;
-  } else if (event === 'tie') {
-    key = 1;
-  }
-  const audio = document.querySelector(`audio[data-key="${key}"]`);
-  if (key === 2) { audio.playbackRate = 2}
-  audio.play();
-}
-
-function resetButtonClick() {
-  reset = document.querySelector('.btn-reset');
-  reset.addEventListener('click', () => {
-    Gameboard.reset();
-    cell.forEach((elem) => elem.style.background = "#A7E399");
-  })
-}
-
-function convertUserChoice() {
+function printSymbol() {
   let marker = 'X'
   cell.forEach((cube) => {
     cube.addEventListener('click', () => {
       const cellID = cube.dataset.id;
       const turnChoice = convertNumberToCell(cellID);
+      const turn = Gameboard.turn(marker, turnChoice.row, turnChoice.col);
+      console.log(turn);
       
-      Gameboard.turn(marker, turnChoice.row, turnChoice.col);
-      printSymbol(marker);
-      marker = marker === 'X' ? 'O' : 'X';
+      if (cube.dataset.filled === 'no') {
+        if (turn === 'X') {cube.style.background = "#A7E399 url('images/x-symbol.svg') no-repeat center center"} else { cube.style.background = "#A7E399 url('images/o-symbol.svg') no-repeat center center";}
+        cube.style.backgroundSize = '80px';
+        cube.dataset.filled = 'yes';
+        console.log(turn);
+        if (turn !== false){marker = marker === 'X' ? 'O' : 'X';};
+      } else {
+        return;
+      }
     })
-  })  
+  })
 }
 
 function convertNumberToCell(idx) {
@@ -145,22 +155,9 @@ function convertNumberToCell(idx) {
   }
 }
 
-function menu() {
-  start = document.querySelector('.start')
-  startBTN = document.querySelector('.btn-start')
-  startBTN.addEventListener('click', () => {
-    start.style.visibility = "hidden";
-})
-}
 
-// function game() {
-//   let bookmark = 'X'
-//   while (bookmark === 'X' || bookmark === 'O') {
-//     convertNumberToCell(bookmark);
-//     bookmark = bookmark === 'X' ? 'O' : 'X';
-//   }
-// }
-menu();
-resetButtonClick();
-turnSound();
-convertUserChoice()
+DOMmanipulation.menu();
+DOMmanipulation.resetButtonClick();
+DOMmanipulation.turnSound();
+printSymbol();
+
